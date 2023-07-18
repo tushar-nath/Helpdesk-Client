42867ed290024b47bd0a124d94ed62a752453633
tusharnath10 <tusharnath10@gmail.com>
2023-07-19 00:33:39 +0530

    Added a protected page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import ProtectedPage from "./components/Protected";
 import "tailwindcss/tailwind.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/protected" element={<ProtectedPage />} />
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Login() {
+  const handleLogin = () => {
+    // Redirect to the Google OAuth URL
+    window.location.href = "http://localhost:3000/api/v1/auth/google";
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-md w-full bg-white p-8 shadow-lg rounded-md text-center">
+        <h1 className="text-3xl font-bold mb-6">Login Page</h1>
+        <button
+          onClick={handleLogin}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Login with Google
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
