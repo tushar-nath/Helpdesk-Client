59896a91bb349bf8b5104e7860aa21bcf499f640
tushar-nath <tusharnath10@gmail.com>
2023-08-06 14:47:00 +0530

    Added a login logo

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Google from "../images/google.png";
 
 const base_url = process.env.REACT_APP_API_BASE_URL;
 
@@ -12,12 +13,16 @@ function Login() {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="max-w-md w-full bg-white p-8 shadow-lg rounded-md text-center">
         <h1 className="text-3xl font-bold mb-6">Login Page</h1>
-        <button
-          onClick={handleLogin}
-          className="bg-black border hover:bg-white hover:text-black hover:border-3 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          Login with Google
-        </button>
+        <div className="flex justify-center">
+          {" "}
+          <button
+            onClick={handleLogin}
+            className="flex items-center bg-black border hover:bg-white hover:text-black hover:border-3 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            <img src={Google} alt="Google Logo" className="h-6 w-6 mr-2" />
+            Login with Google
+          </button>
+        </div>
       </div>
     </div>
   );
