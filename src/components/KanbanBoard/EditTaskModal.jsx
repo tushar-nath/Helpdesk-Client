c0943576aafe38db403eb828dcc04805a4b7fdaf
tushar-nath <tusharnath10@gmail.com>
2023-08-10 13:59:20 +0530

    chore: updated the edit modal popup to improve UI/UX experience

--- src/components/KanbanBoard/EditTaskModal.jsx
+++ src/components/KanbanBoard/EditTaskModal.jsx
@@ -22,6 +22,17 @@ const EditTaskModal = ({ isOpen, onClose, selectedTask, onSave }) => {
     "Marketing",
   ];
 
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  // Editable state for each field
+  const [isTitleEditable, setIsTitleEditable] = useState(false);
+  const [isDescriptionEditable, setIsDescriptionEditable] = useState(false);
+  const [isStatusEditable, setIsStatusEditable] = useState(false);
+  const [isDueDateEditable, setIsDueDateEditable] = useState(false);
+
   const handleSave = () => {
     // Perform validation to check if all the required fields are filled
     if (!title || !description || !status || !dueDate) {
@@ -70,48 +81,68 @@ const EditTaskModal = ({ isOpen, onClose, selectedTask, onSave }) => {
       className="w-96 mx-auto p-6 rounded-lg shadow-lg bg-white"
       overlayClassName="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50"
     >
-      <h2 className="text-xl font-bold mb-4">Edit Task</h2>
+      <h2 className="text-xl font-bold mb-4">
+        {isTitleEditable ? (
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            onBlur={() => setIsTitleEditable(false)}
+            autoFocus
+          />
+        ) : (
+          <span onClick={() => setIsTitleEditable(true)}>{title}</span>
+        )}
+      </h2>
       {error && <p className="text-red-600 mb-4">{error}</p>}
       <div className="mb-4">
-        <label className="block font-medium">Title:</label>
-        <input
-          className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500"
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+        <div className="flex justify-between items-center mb-2">
+          <span className="font-bold">Ticket Type:</span>
+          {isStatusEditable ? (
+            <select
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+              onBlur={() => setIsStatusEditable(false)}
+            >
+              <option value="">Select Ticket Type</option>
+              {ticketTypes.map((type) => (
+                <option key={type} value={type}>
+                  {type}
+                </option>
+              ))}
+            </select>
+          ) : (
+            <span onClick={() => setIsStatusEditable(true)}>{status}</span>
+          )}
+        </div>
+        <div className="flex justify-between items-center mb-2">
+          <span className="font-bold">Due Date:</span>
+          {isDueDateEditable ? (
+            <input
+              type="date"
+              value={dueDate}
+              onChange={(e) => setDueDate(e.target.value)}
+              onBlur={() => setIsDueDateEditable(false)}
+            />
+          ) : (
+            <span onClick={() => setIsDueDateEditable(true)}>
+              {formatDate(dueDate)}
+            </span>
+          )}
+        </div>
       </div>
       <div className="mb-4">
-        <label className="block font-medium">Description:</label>
-        <textarea
-          className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </div>
-      <div className="mb-4">
-        <label className="block font-medium">Ticket Type:</label>
-        <select
-          className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500"
-          value={status}
-          onChange={(e) => setStatus(e.target.value)}
-        >
-          <option value="">Select Ticket Type</option>
-          {ticketTypes.map((type) => (
-            <option key={type} value={type}>
-              {type}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="mb-4">
-        <label className="block font-medium">Due Date:</label>
-        <input
-          className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500"
-          type="date"
-          value={dueDate}
-          onChange={(e) => setDueDate(e.target.value)}
-        />
+        <p className="font-bold mb-2">Description:</p>
+        {isDescriptionEditable ? (
+          <textarea
+            className="w-full border-none rounded-md focus:outline-none focus:border-blue-500"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            onBlur={() => setIsDescriptionEditable(false)}
+          />
+        ) : (
+          <p onClick={() => setIsDescriptionEditable(true)}>{description}</p>
+        )}
       </div>
       <button
         className="rounded-[5px] bg-[#141414] text-white py-2 px-4 rounded-md mr-2"
